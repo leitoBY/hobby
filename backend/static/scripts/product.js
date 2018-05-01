ea3c0975ec2cadbc4d693e0c2cbba7adabb92a08
leitoBY <freeleito@mail.ru>
2018-05-02 02:19:35 +0300

    Add script file for product page. Some changes to templates

---
+++ backend/static/scripts/product.js
@@ -0,0 +1,8 @@
+console.log("js script for product page")
+
+// add ajax for adding product to local storage, then will provide this info for purchase page
+let add_to_cart_button = document.getElementById("add_to_cart");
+
+add_to_cart_button.onclick = function addNewUser() {
+    console.log("pruduct adde to cart")
+};

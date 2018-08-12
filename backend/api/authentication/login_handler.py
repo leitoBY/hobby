a17581b3a1e3b783c7aded2bf7d0ea9e9b6d61cd
freeleito@mail.ru <freeleito@mail.ru>
2018-08-13 01:02:37 +0300

    change setting cookies to server-side

--- backend/api/authentication/login_handler.py
+++ backend/api/authentication/login_handler.py
@@ -1,6 +1,5 @@
-from flask import jsonify, request, render_template, Blueprint
+from flask import jsonify, request, render_template, Blueprint, make_response, redirect
 from api.authentication.login_service import LoginService
-
 
 login_api = Blueprint('login_api', __name__)
 @login_api.route('/login', methods=['GET', 'POST'])
@@ -13,7 +12,10 @@ def login_user():
         try:
             jwt_token = LoginService.login(data=body)
             if jwt_token:
-                return jsonify({"jwt_token": jwt_token})
+                response = make_response(redirect('/'))
+                response.set_cookie('jwt_token', jwt_token)
+                return response
+                # return jsonify({"jwt_token": jwt_token})
             return jsonify(message="Something went wrong")
         except Exception as e:
             return jsonify(error=str(e))
--- backend/static/scripts/main.js
+++ backend/static/scripts/main.js
@@ -44,39 +44,8 @@ if (login_button) {
                 headers: new Headers({
                     'Content-Type': 'application/json'
                 })
-            }).then((server_response) => {
-                server_response.json().then((data) => {
-                    let jwt_token = data.jwt_token;
-                    window.localStorage.jwt_token = data.jwt_token;
-                    setCookie('jwt_token', data.jwt_token, 7);
-                    window.location.replace("/");
-                });
+            }).then(() => {
+                window.location.replace("/");
             });
         };
     }
-
-function setCookie(name, value, days) {
-    console.log('sett new cookie');
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "") + expires + "; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
-}
